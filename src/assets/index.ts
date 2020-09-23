6cddba58060de8cde285e0e374824075d24a772c
gimtommang11 <gimtommang88@gmail.com>
2020-09-23 23:05:04 +0900

    import assets

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import purple_footprint from "./footprint/purple_footprint.png";
+import white_footprint from "./footprint/white_footprint.png";
+
+export const footprint = {
+  purple_footprint,
+  white_footprint,
+};
