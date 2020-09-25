ba70f2a336c09fdef7972509a6a26729035d552a
gimtommang11 <gimtommang88@gmail.com>
2020-09-25 12:07:29 +0900

    add assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,18 @@
 import purple_footprint from "./footprint/purple_footprint.png";
 import white_footprint from "./footprint/white_footprint.png";
+import cat from "./sidebar/cat.png";
+import home from "./sidebar/home.png";
+import user from "./sidebar/user.png";
+import logo from "./sidebar/logo.png";
 
 export const footprint = {
   purple_footprint,
   white_footprint,
+};
+
+export const sidebar = {
+  cat,
+  home,
+  user,
+  logo,
 };
